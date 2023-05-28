24a56a672afc7cb5d79d04a139c9ecfee61fe32d
Sagar Karandikar <sagark@eecs.berkeley.edu>
2023-05-28 11:49:28 -0700

    switch to TCL-based flow

--- Sample-Projects/Project-0/cl_firesim/Nitefury-II/project/top.tcl
+++ Sample-Projects/Project-0/cl_firesim/Nitefury-II/project/top.tcl
@@ -1,9 +1,9 @@
 
 
-open_project project.xpr
+set ifrequency              [lindex $argv 0]
+set istrategy               [lindex $argv 1]
 
-
-source ../../common/Scripts/rebuild.tcl
-
-
+source project.tcl
+cd project
+source ../../../common/Scripts/rebuild.tcl
 close_project
